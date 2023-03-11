b3bc0bbe6f7fbf52698f93cb85a997669dc40d56
Sascha Steger <s-steger@live.de>
2023-03-11 18:14:17 +0100

    Plot reward shaping vs no reward shaping

--- print_plot_log/plot_total_train.py
+++ print_plot_log/plot_total_train.py
@@ -27,6 +27,27 @@ class PlotTraining:
         plt.title('Rewards during Training')
         plt.plot(x_axis, results)
         plt.savefig('diagramms/Train.png')
+    
+    def plot_reward_shaping_vs_no_reward_shaping():
+        """Plot the reward of the model with reward shaping 
+           and the model without reward shaping"""
+        
+        logger_reward_shaping = Logger('models_with_reward_shaping')
+        logger_no_reward_shaping = Logger('models_without_reward_shaping')
+        
+        results_reward_shaping = logger_reward_shaping.get_all_rewards()
+        results_no_reward_shaping = logger_no_reward_shaping.get_all_rewards()
+        
+        x_axis = [x for x in range(len(results_reward_shaping))]
+        
+        plt.ylabel('Rewards pro Epoche')
+        plt.xlabel('Epoche')
+        plt.title('Rewards during Training')
+        plt.plot(x_axis, results_reward_shaping, label='reward shaping')
+        plt.plot(x_axis, results_no_reward_shaping, label='no reward shaping')
+        plt.legend()
+        plt.savefig('diagramms/Reward Shaping vs No Reward Shaping.png')
+
 
 
 def main():
